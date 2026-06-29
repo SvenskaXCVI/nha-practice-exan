6b20d232e812683e08211da64de24fafccd3b5fb
SvenskaXCVI <nutdanielquist@gmail.com>
2026-06-28 21:15:51 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nha-mobile-practice-v4';
+const CACHE_NAME = 'nha-mobile-practice-v5';
 const APP_ASSETS = ['./','./index.html','./manifest.webmanifest','./icons/icon-192.svg','./icons/icon-512.svg'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); self.clients.claim(); });
